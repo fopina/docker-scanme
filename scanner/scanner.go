b05b84206512afca2b7da79674f3e0538a3c3bae
Pina <fopina@gmail.com>
2020-12-31 15:03:36 +0000

    typo

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -67,7 +67,6 @@ func (s *Scanner) notify(message string) bool {
 		url.Values{"msg": {message}, "format": {"Markdown"}},
 	)
 	if err != nil {
-		log.Println(resp)
 		log.Println(err)
 		return false
 	}
